79a68bfc4a7488d43de15c59271614640f659388
kondrat1206 <kondrat1206@gmail.com>
2023-11-09 16:55:57 +0200

    init start

---
+++ classes.py
@@ -0,0 +1,281 @@
+from prompt_toolkit.completion import Completer, Completion
+from collections import UserDict
+from datetime import datetime, date
+import re
+
+
+
+class AddressBook(UserDict):
+
+    def __init__(self):
+
+        self.data = {}
+        self.current_page = 0
+        self.page_size = 0
+
+
+    def set_page_size(self, page_size):
+
+        self.page_size = page_size
+
+
+    def __iter__(self):
+
+        self.current_page = 0
+        
+        return self
+    
+
+    def __next__(self):
+        
+        start_idx = self.current_page * self.page_size
+        end_idx = (self.current_page + 1) * self.page_size
+        items = list(self.data.items())
+
+        if start_idx >= len(items):
+            raise StopIteration
+
+        self.current_page += 1
+
+        return items[start_idx:end_idx]
+    
+
+    def get_pages(self, page_size=100):
+
+        self.page_size = page_size
+        self.set_page_size(self.page_size)
+
+        for page in self:
+            phone_list = []
+            print(f"---------------PAGE-{self.current_page}---------------------")
+            for el in page:
+                for i in el[1].phones:
+                    phone_list.append(i.value)
+                print(f"Name: {el[0]} Phones: {phone_list} Birthday: [{el[1].birthday.value}]")
+            print(f"---------------END-PAGE-{self.current_page}-----------------")
+
+        return "Completed"
+
+
+    def is_contact_exist(self, record):
+
+        keys = []
+        for key in self.data.keys():
+            keys.append(key)
+        if record.name.value in keys:
+            result = True
+        else:
+            result = False
+            
+        return result
+    
+
+    def is_phone_exist(self, record):
+
+        num_values = []
+        for  i in self.data[record.name.value].phones:
+            num_values.append(i.value)
+        if record.phones[0].value in num_values:
+            result = True
+        else:
+            result = False
+
+        return result
+
+
+    def add_record(self, record):
+
+        if len(record.phones) < 1:
+            if self.is_contact_exist(record) == False:
+                self.record = record
+                self.data[self.record.name.value] = self.record
+                result = f"Contact \"{record.name.value}\" added to address book without phone\n"
+            else:
+                result = f"Contact \"{record.name.value}\" already exists in address book\n"
+        else:
+            if self.is_contact_exist(record) == False:
+                self.record = record
+                self.data[self.record.name.value] = self.record
+                result = f"Contact \"{record.name.value}\" added to address book with phone \"{record.phones[0].value}\"\n"
+            else:
+                if self.is_phone_exist(record):
+                    result = f"Phone \"{record.phones[0].value}\" already exists into the contact \"{record.name.value}\"\n"
+                else:
+                    self.data[self.record.name.value].add_phone(record.phones[0])
+                    result = f"Added new phone \"{record.phones[0].value}\" to contact \"{record.name.value}\"\n"
+
+        return result
+
+
+class Record:
+
+    def __init__(self, name, phone=None, birthday=None): 
+
+        self.name = name
+        self.birthday = birthday
+        self.phones = []
+        if phone != None:
+            self.phones.append(phone)
+
+    
+    def add_phone(self, phone):
+
+        self.phones.append(phone)
+        result = f'Number \"{phone.value}\" added to phone list of \"{self.name.value}\"\n'
+
+        return result
+    
+
+    def add_birthday(self, birthday):
+
+        self.birthday.value = birthday.value
+        result = f'Value \"{self.birthday.value}\" added to birthday of \"{self.name.value}\"\n'
+
+        return result
+
+
+    def remove_phone(self, phone):
+        
+        phone_num = phone.value
+        for id, obj in enumerate(self.phones):
+            if obj == phone:
+                self.phones.pop(id)
+                result = f'Number {phone_num} remuved from phone list of {self.name.value}'
+                break
+                
+        return result
+    
+
+    def change_phone(self, old_phone, new_phone):
+
+        phone_values = []
+        for number in self.phones:
+            phone_values.append(number.value)
+        if old_phone.value in phone_values:
+            for index, obj in enumerate(self.phones):
+                if obj.value == old_phone.value:
+                    self.phones[index] = new_phone
+                    break
+            result = f"Number \"{old_phone.value}\" changed to \"{new_phone.value}\" in phone list of \"{self.name.value}\"\n"
+        else:
+            result = f"Phone \"{old_phone.value}\" does not exist in the phone list of contact \"{self.name.value}\"\n"
+                   
+        return result
+    
+
+    def days_to_birthday(self):
+
+        if self.birthday.value == None:
+            result = f"Birthday value not set of contact \"{self.name.value}\"\nYou can set the Birthday with command:\nadd birthday {self.name.value} DD.MM.YYYY\n"
+        else:
+            now = date.today()
+            birthday = datetime.strptime(self.birthday.value, "%d.%m.%Y").date()
+            if now.month > birthday.month or (now.month == birthday.month and now.day >= birthday.day):
+                next_birthday = date(now.year + 1, birthday.month, birthday.day)
+            else:
+                next_birthday = date(now.year, birthday.month, birthday.day)
+
+            days = (next_birthday - now).days
+            result = f"To Birthday of contact \"{self.name.value}\" untill \"{days}\" days\n"
+
+        return result
+
+
+class Field:
+    
+    def __init__(self, value):
+
+        self.__value = value
+
+    @property
+    def value(self):
+
+        return self.__value
+
+    
+    @value.setter
+    def value(self, value):
+        check = self.check_value(value)
+        if check == True:
+            self.__value = value
+        else:
+            raise ValueError(check)
+        
+    
+class Name(Field):
+
+    def check_value(self, value):
+       
+        return True
+    
+
+class Phone(Field):
+
+    def check_value(self, value):
+        match = re.fullmatch(r'\+\d{12}', value)
+        if match:
+            result = True
+        else:
+            result =  f"""Entered value \"{value}\" is not correct.\nPhone must start with \"+\" and must have 12 digits.\nFor example: \"+380681235566\"\n\nTRY AGAIN!!!"""
+       
+        return result
+    
+
+class Birthday(Field):
+
+    def check_value(self, value):
+
+        match = re.fullmatch(r'\d{2}\.\d{2}\.\d{4}', value)
+        if match:
+            result = True
+        else:
+            result =  f"""Entered value \"{value}\" is not correct.\nBirthday must have a format: DD.MM.YYYY and contain only numbers\nFor example: \"12.06.1978\"\n\nTRY AGAIN!!!"""
+        
+        day, month, year = map(int, value.split('.'))
+        try:
+            datetime(year, month, day)
+        except ValueError:
+            result = f"""Entered value \"{value}\" is not correct.\nSetted date is not exist\n\nTRY AGAIN!!!"""
+
+        current_date = datetime.now().date()
+        if datetime(year, month, day).date() > current_date:
+            result = f"""Entered value \"{value}\" is not correct.\nSetted date can not be at the future\n\nTRY AGAIN!!!"""
+        
+        return result
+    
+
+class MyCompleter(Completer):
+    
+    def get_completions(self, document, complete_event):
+
+        commands_list = ['good bye', 'close', 'exit', 'show all', 'hello', 'add birthday', 'add', 'change', 'phone', 'to birthday', 'help', 'pages', 'search']
+        users = [AddressBook.keys]
+        text = document.text
+        completions = []
+        for command in commands_list:
+            if text in command:  
+                completions = [c for c in commands_list if text in c]
+                
+            # elif text.startswith('add birthday'):
+            #     completions = [text.rsplit(' ', 1)[0]+' '+u for u in options if u.startswith(text.split()[-1])]
+                
+            # elif text.startswith('add'):
+            #     completions = [text.rsplit(' ', 1)[0]+' '+p for p in phones if p.startswith(text.split()[-1])]
+                
+            # elif text.startswith('change'):
+            #     completions = [text.rsplit(' ', 1)[0]+' '+b for b in birthdays if b.startswith(text.split()[-1])]
+
+            # elif text.startswith('phone'):
+            #     completions = [text.rsplit(' ', 1)[0]+' '+b for b in birthdays if b.startswith(text.split()[-1])]
+
+            # elif text.startswith('to birthday'):
+            #     completions = [text.rsplit(' ', 1)[0]+' '+b for b in users]
+
+            elif text.startswith('pages'):
+                completions = [text.rsplit(' ', 1)[0]+' '+'DIGIT']
+
+            elif text.startswith('search'):
+                completions = [text.rsplit(' ', 1)[0]+' '+'STRING']
+                
+        for completion in completions:
+            yield Completion(completion, start_position=-len(text))
---
+++ main.py
@@ -0,0 +1,342 @@
+from prompt_toolkit import prompt
+from classes import AddressBook, Record, Name, Phone, Birthday, MyCompleter
+import functools
+import pickle
+import os
+
+help = """
+Available commands:
+hello: print \"How can I help you?\"
+add [name] [phone] [birthday]: Add a new record to address book or new phone to contact phone list
+add birthday [name] [birthday]: Add a new/change Birthday to the contact of address book
+to birthday [name]: Show days to contact`s Birthday
+change [name] [old_phone] [new_phone]: Change phone num for contact in address book
+phone [name]: Show phone list of contact
+show all: Show address book
+pages [size]: Show address book in pages, size is number records per page
+search [string]: Matching search for name or phone in address book
+good bye, close, exit: print \"Good bye!\" and exit
+help: Show this help
+"""
+
+address_book = AddressBook()
+data_file = "address_book.bin"
+
+
+def input_error(func):
+
+    @functools.wraps(func)
+    def inner(param_list):
+
+        if func.__name__ == "phone":
+            if len(param_list) > 0:
+                result = func(param_list)
+            else:
+                result = f"""Command \"{func.__name__}\" reqired 1 argument: name.\nFor example: {func.__name__} [name]\n\nTRY AGAIN!!!"""
+        elif func.__name__ == "add":
+            if len(param_list) > 0:
+                result = func(param_list)
+            else:
+                result = f"""Command \"{func.__name__}\" reqired 1 or 2 arguments: name and phone.\nFor example: {func.__name__} [name] - To add a new contact without phones\nFor example: {func.__name__} [name] [phone] - To add a new contact with phones, or add new phone to contact\n\nTRY AGAIN!!!"""
+        elif func.__name__ == "add_birthday":
+            param_list.pop(0)
+            if len(param_list) > 1:
+                result = func(param_list)
+            else:
+                result = f"""Command \"{func.__name__.replace("_", " ")}\" reqired 2 arguments: name and birthday.\nFor example: {func.__name__.replace("_", " ")} [name] [birthday]\n\nTRY AGAIN!!!"""
+
+        elif func.__name__ == "change":
+            if len(param_list) > 2:
+                result = func(param_list)
+            else:
+                result = f"""Command \"{func.__name__}\" reqired 3 arguments: name, phone and new_phone.\nFor example: {func.__name__} [name] [phone] [new_phone]\n\nTRY AGAIN!!!"""
+        elif func.__name__ == "to_birthday":
+            param_list.pop(0)
+            if len(param_list) > 0:
+                result = func(param_list)
+            else:
+                result = f"""Command \"{func.__name__.replace("_", " ")}\" reqired 1 argument: name.\nFor example: {func.__name__.replace("_", " ")} [name]\n\nTRY AGAIN!!!"""
+        elif func.__name__ == "pages":
+            if len(param_list) > 0 and int(param_list[0]) > 0:
+                result = func(param_list)
+            else:
+                result = f"""Command \"{func.__name__}\" reqired 1 argument: size. Size int and must be > 0.\nFor example: {func.__name__} [size]\n\nTRY AGAIN!!!"""
+        elif func.__name__ == "search":
+            if len(param_list) > 0:
+                result = func(param_list)
+            else:
+                result = f"""Command \"{func.__name__}\" reqired 1 argument: search string. \nFor example: {func.__name__} [string]\n\nTRY AGAIN!!!"""
+
+        return result
+    return inner
+
+
+@input_error
+def add(param_list):
+
+    name = Name(None)
+    phone = Phone(None)
+    birthday = Birthday(None)
+
+    if len(param_list) == 1:
+        try:
+            name.value = param_list[0]
+        except ValueError as e:
+            result = str(e)
+            return result
+    elif len(param_list) == 2:
+        try:
+            name.value = param_list[0]
+        except ValueError as e:
+            result = str(e)
+            return result
+        try:
+            phone.value = param_list[1]
+        except ValueError as e:
+            result = str(e)
+            return result
+    elif len(param_list) > 2:
+        try:
+            name.value = param_list[0]
+        except ValueError as e:
+            result = str(e)
+            return result
+        try:
+            phone.value = param_list[1]
+        except ValueError as e:
+            result = str(e)
+            return result
+        try:
+            birthday.value = param_list[2]
+        except ValueError as e:
+            result = str(e)
+            return result
+
+    if phone.value != None:
+        result = address_book.add_record(
+            Record(name, phone=phone, birthday=birthday))
+    else:
+        result = address_book.add_record(Record(name, birthday=birthday))
+
+    return result
+
+
+@input_error
+def add_birthday(param_list):
+
+    name = Name(None)
+    birthday = Birthday(None)
+    record = Record(Name(param_list[0]))
+    if address_book.is_contact_exist(record):
+        try:
+            name.value = param_list[0]
+        except ValueError as e:
+            result = str(e)
+            return result
+        try:
+            birthday.value = param_list[1]
+        except ValueError as e:
+            result = str(e)
+            return result
+
+        result = address_book[param_list[0]].add_birthday(birthday)
+    else:
+        result = f"Contact \"{param_list[0]}\" does not exist in the address book\n"
+
+    return result
+
+
+@input_error
+def change(param_list):
+
+    record = Record(Name(param_list[0]))
+    old_phone_obj = Phone(param_list[1])
+    new_phone_obj = Phone(None)
+    try:
+        new_phone_obj.value = param_list[2]
+    except ValueError as e:
+        result = str(e)
+        return result
+
+    if address_book.is_contact_exist(record):
+        result = address_book[param_list[0]].change_phone(
+            old_phone_obj, new_phone_obj)
+    else:
+        result = f"Contact \"{param_list[0]}\" does not exist in the address book\n"
+
+    return result
+
+
+@input_error
+def phone(param_list):
+
+    name = param_list[0]
+    record = Record(Name(param_list[0]), None)
+    if address_book.is_contact_exist(record):
+        phone_list = address_book[name].phones
+        value_list = []
+        for phone_obj in phone_list:
+            value_list.append(phone_obj.value)
+        result = f"Phone list of contact \"{name}\" is \"{value_list}\"\n"
+    else:
+        result = f"Contact \"{name}\" does not exist in the address book\n"
+
+    return result
+
+
+@input_error
+def to_birthday(param_list):
+
+    record = Record(Name(param_list[0]))
+    if address_book.is_contact_exist(record):
+        result = address_book[param_list[0]].days_to_birthday()
+    else:
+        result = f"Contact \"{param_list[0]}\" does not exist in the address book\n"
+
+    return result
+
+
+def hello(param_list):
+
+    result = "How can I help you?\n"
+    return result
+
+
+def exit(param_list):
+
+    result = "exit"
+    return result
+
+
+def show_all(param_list):
+
+    result = "All contacts:\n"
+    for name, record in address_book.data.items():
+        phones = record.phones
+        birthday = record.birthday.value
+        phone_values = []
+        for phone in phones:
+            phone_values.append(phone.value)
+        result += f"Name: \"{name}\", Phones: {phone_values}, Birthday: [{birthday}]\n"
+
+    return result
+
+
+@input_error
+def pages(param_list):
+    size = int(param_list[0])
+    result = address_book.get_pages(page_size=size)
+
+    return result
+
+
+@input_error
+def search(param_list):
+
+    result = "Finded contacts:\n"
+    search_string = param_list[0]
+    for key, value in address_book.data.items():
+        first_string = f"{key}"
+        phones_string = ""
+        phone_values = []
+        for phone in value.phones:
+            phones_string = phones_string + str(phone.value)
+            phone_values.append(phone.value)
+        full_string = first_string + phones_string
+        if search_string in full_string:
+            result += f"Name: \"{key}\", Phones: {phone_values}, Birthday: [{value.birthday.value}]\n"
+
+    return result
+
+
+def helper(param_list):
+
+    return help
+
+
+def save_data():
+
+    with open(data_file, "wb") as file:
+        pickle.dump(address_book, file)
+
+
+def load_data():
+    with open(data_file, 'rb') as file:
+        address_book = pickle.load(file)
+
+    return address_book
+
+
+commands = {
+    "good bye": exit,
+    "close": exit,
+    "exit": exit,
+    "show all": show_all,
+    "show_all": show_all,
+    "hello": hello,
+    "add birthday": add_birthday,
+    "add_birthday": add_birthday,
+    "add": add,
+    "change": change,
+    "phone": phone,
+    "to birthday": to_birthday,
+    "to_birthday": to_birthday,
+    "help": helper,
+    "helper": helper,
+    "pages": pages,
+    "search": search
+}
+
+
+def parser(string: str):
+
+    lower_string = string.lower()
+    for keyword, command in commands.items():
+        if keyword in lower_string:
+            param_list = string.split()
+            for param in param_list:
+                if param.lower() in keyword:
+                    param_list.remove(param)
+                    command = command.__name__
+            return command, param_list
+
+    return None, ""
+
+
+def handler(command):
+
+    return commands[command]
+
+
+def main():
+
+    # comm_completer = list(commands.keys())
+    # filtered_commands = [key for key in list(commands.keys()) if "_" not in key and key != "helper"]
+
+    # result_dict = {key: None for key in filtered_commands}
+
+    # completer = WordCompleter(filtered_commands)
+    # print(filtered_commands)
+    while True:
+        # source_command = input("Enter command: ")
+        #print(address_book.data)
+        source_command = prompt("Enter command: ", completer=MyCompleter())
+        command, param_list = parser(source_command)
+        if not command:
+            print(f"YOU ENTERED A WRONG COMMAND!!!\n{help}\nTRY AGAIN!!!")
+            continue
+        result = handler(command)(param_list)
+        if result == 'exit':
+            print("Good bye!")
+            break
+        else:
+            print(result)
+            save_data()
+
+
+if __name__ == "__main__":
+
+    if os.path.exists(data_file):
+        address_book = load_data()
+
+    main()

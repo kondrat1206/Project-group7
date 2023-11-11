a316a7d9b2b4dae8b6340be05292b6fae8e7659b
kondrat1206 <kondrat1206@gmail.com>
2023-11-11 10:00:09 +0200

    add notes

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import functools
 import pickle
 import os
 from sortfolder import check_args, list_files_recursive, sort, unpack_archives, normalize, remove_empty_directories, library
+from project_notes import PersonalAssistant
 
 help = """
 Available commands:
@@ -17,6 +18,7 @@ show all: Show address book
 pages [size]: Show address book in pages, size is number records per page
 search [string]: Matching search for name or phone in address book
 sort folder [path to folder]: Sort files depends extensions into the target folder
+notes: module for notes use
 good bye, close, exit: print \"Good bye!\" and exit
 help: Show this help
 """
@@ -303,6 +305,44 @@ def sort_folder(param_list):
 
     remove_empty_directories(new_dir)
     print(f'Ваши файлы отсортированы в директории {new_dir}, пустые папки удалены')
+
+
+def notes (param_list):
+    
+    assistant = PersonalAssistant()
+    while True:
+        print("Select an option:")
+        print("1. Add note")
+        print("2. List of notes")
+        print("3. Find note")
+        print("4. Edit note")
+        print("5. Delete note")
+        print("0. Exit")
+        
+        choice = input("Choose an option: ")
+        
+        if choice == "1":
+            text = input("Input text of note: ")
+            tags = input("Input tags (use ','): ").split(",")
+            assistant.add_note(text, [tag.strip() for tag in tags])
+        elif choice == "2":
+            assistant.list_notes()
+        elif choice == "3":
+            query = input("Enter a query to search for notes: ")
+            assistant.search_notes(query)
+        elif choice == "4":
+            note_id = int(input("Enter the note number you want to edit: "))
+            new_text = input("New note text: ")
+            new_tags = input("New tags (separated by commas): ").split(",")
+            assistant.edit_note(note_id, new_text, [tag.strip() for tag in new_tags])
+        elif choice == "5":
+            note_id = int(input("Enter the note number you want to delete: "))
+            assistant.delete_note(note_id)
+        elif choice == "0":
+            assistant.save_to_json()
+            break
+        else:
+            print("Incorrect choice. Please try again.")
     
 
 
@@ -325,7 +365,8 @@ commands = {
     "pages": pages,
     "search": search,
     "sort folder": sort_folder,
-    "sort_folder": sort_folder
+    "sort_folder": sort_folder,
+    "notes": notes
 }
 
 

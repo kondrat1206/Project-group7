982edea888af675dfb628a8307d20af7397968b4
kondrat1206 <kondrat1206@gmail.com>
2023-11-13 04:49:21 +0200

    fix

--- assistant/main.py
+++ assistant/main.py
@@ -1,10 +1,10 @@
 from prompt_toolkit import prompt
-from assistant.classes import AddressBook, Record, Name, Phone, Birthday, MyCompleter
+from classes import AddressBook, Record, Name, Phone, Birthday, MyCompleter
 import functools
 import pickle
 import os
-from assistant.sortfolder import check_args, list_files_recursive, sort, unpack_archives, normalize, remove_empty_directories, library
-from assistant.project_notes import PersonalAssistant
+from sortfolder import check_args, list_files_recursive, sort, unpack_archives, normalize, remove_empty_directories, library
+from project_notes import PersonalAssistant
 import re
 
 help = """
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 setup(
     name='assistant',
     version='0.1.0',
-    entry_points={
-        'console_scripts': [
-            'assistant = assistant.main:main'
-        ]
-    }
+    description='Very useful code',
+    author='7Team',
+    license='MIT',
+    packages=find_namespace_packages(),
+    entry_points={'console_scripts': ['assistant = assistant.main:main']}
 )
